f19ac6305037f8ccf0f66ab0615faea94df50d06
konsultan.it <konsulkan.it@gmail.com>
2024-01-29 11:40:27 +0700

    mesin tsmilk awal

---
+++ Core/Inc/logo.h
@@ -0,0 +1,16 @@
+/*
+ * logo.h
+ *
+ *  Created on: Jan 29, 2024
+ *      Author: NAMRI
+ */
+
+#ifndef INC_LOGO_H_
+#define INC_LOGO_H_
+
+#include <stdint.h>
+//extern const uint16_t logo[76800];
+extern const unsigned char logo [];
+extern const uint32_t image_data_logo2[38400];
+
+#endif /* INC_LOGO_H_ */
